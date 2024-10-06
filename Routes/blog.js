b782c459884efb484cf9afdc82e0758bcc87c31d
Owais Nazir <owaisaalam38@gmail.com>
2024-10-06 22:04:38 +0530

    Initial commit

--- Routes/blog.js
+++ Routes/blog.js
@@ -7,7 +7,7 @@ const comment = require('../model/commentschema');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../public/images/uploads'))
+    cb(null, path.join(__dirname, 'public/images/uploads'))
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now()
